9501c6f8e1db299fd56c2de46bd2e8d1a0bbcf11
efe ömer şakar <sakarefeomer@gmail.com>
2025-02-17 23:28:22 +0300

    CameraShake

--- Assets/Scripts/InGame/CameraControl.cs
+++ Assets/Scripts/InGame/CameraControl.cs
@@ -6,6 +6,15 @@ public class CameraControl : MonoBehaviour
     [SerializeField] private Transform Target;
     [SerializeField] private float smoothTime = 0.5f;
     private Vector3 velocity = Vector3.zero;
+    void OnEnable()
+    {
+        EventManager.Instance.OnPlayerKilled += PlayerDeathShaking;
+    }
+    void OnDisable()
+    {
+        EventManager.Instance.OnPlayerKilled -= PlayerDeathShaking;
+
+    }
 
     private void LateUpdate()
     {
@@ -14,6 +23,11 @@ public class CameraControl : MonoBehaviour
             Vector3 targetPosition = new Vector3(Target.position.x, Target.position.y, transform.position.z);
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
+
+    }
+    private void PlayerDeathShaking()
+    {
+        transform.DOShakePosition(1f, 0.5f, 20, 90f, false, true);
 
     }
 }
